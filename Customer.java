709bf7a906620a1fc48bba9de3a186da757e1094
Ami1423 <63359009+Ami1423@users.noreply.github.com>
2020-06-17 10:33:51 -0400

    This change was done for some important purpose
    Ticket#1154:Add another class

---
+++ Customer.java
@@ -0,0 +1,36 @@
+package com.web.CustomerRestClient;
+
+public class Customer {
+
+	private int acctNum;
+	private String name;
+	private double balance;
+	private String type;
+	
+	public int getAcctNum() {
+		return acctNum;
+	}
+	public void setAcctNum(int acctNum) {
+		this.acctNum = acctNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
